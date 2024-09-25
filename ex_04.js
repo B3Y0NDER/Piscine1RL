e7403259825a14cf7e49a27bfb923c77a554136f
B3Y0NDER <dine.terzi@epitech.digital>
2024-09-25 10:10:08 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,6 @@
+document.body.innerHTML = `
+    <h1>Bienvenue dans le pool de javascript!</h1>
+    <p>Amusez-vous et n’oubliez pas d’apporter une serviette.</p>
+`;
+
+console.log("Lancement du site Web!");
